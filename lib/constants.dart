3255504d34174a0e063682ff4903c5a68d9729f0
ali-susanto <alisusanto.main@gmail.com>
2022-05-28 14:17:55 +0700

    add color pick and file login

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryPurple = Color(0xFF50266A);
+const kSecondaryPurple = Color(0xFF794B93);
+const kPrimaryWhite = Color(0xFFFFFFFF);
+const kSecondaryWhite = Color(0xFFF3F3F3);
