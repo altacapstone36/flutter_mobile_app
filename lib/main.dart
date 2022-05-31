e97138fe1e04e328dd59428f860c7638e0397e65
ali-susanto <alisusanto.main@gmail.com>
2022-05-31 07:07:43 +0700

    edit primary color to purple

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: 'Nunito',
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: const LandingPage(),
     );
