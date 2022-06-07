0b928c5195749beb6d657c6fcf62b503591e5171
ali-susanto <alisusanto.main@gmail.com>
2022-06-07 17:26:17 +0700

    update file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryPurple = Color(0xFF50266A);
-const kSecondaryPurple = Color(0xFF794B93);
-const kPrimaryWhite = Color(0xFFFFFFFF);
-const kSecondaryWhite = Color(0xFFF3F3F3);
+//const kPrimaryPurple = Color(0xFF50266A);
+const kPrimaryColor = Color(0xFF794B93);
+const kSecondaryColor = Color(0xFFF3F3F3);
+const kDarkColor = Color(0xFF5B5B5B);
