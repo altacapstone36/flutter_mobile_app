aebf18bbde5b3c442f8624a5ec5cb47b949806f9
ali-susanto <alisusanto.main@gmail.com>
2022-06-09 16:15:08 +0700

    add screen outpatient

---
+++ lib/screen/outpatient/outpatient_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hospital_management/components/bottom_nav_bar.dart';
+import 'package:hospital_management/enums.dart';
+import 'package:hospital_management/screen/outpatient/components/outpatient_data.dart';
+
+import '../../constants.dart';
+
+class OutpatientScreen extends StatelessWidget {
+  const OutpatientScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Outpatient',
+          style: TextStyle(fontWeight: FontWeight.w600, color: kPrimaryColor),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Column(
+          children: const [OutpatientData()],
+        ),
+      ),
+      bottomNavigationBar:
+          const BottomNavBar(selectedMenu: MenuState.outpatient),
+    );
+  }
+}
