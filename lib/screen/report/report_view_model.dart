1b051721b006182a90d88c67e34b879480fcc066
ali-susanto <alisusanto.main@gmail.com>
2022-07-05 13:45:29 +0700

    add report view model

--- lib/screen/report/report_view_model.dart
+++ lib/screen/report/report_view_model.dart
@@ -1,11 +1,19 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hospital_management/api/report_api.dart';
 import 'package:hospital_management/model/report/report_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../enums.dart';
+import '../../model/error/error_model.dart';
+import '../../model/user/model_user.dart';
 
 class ReportViewModel with ChangeNotifier {
-  final List<ReportModel> _report = [];
-  List<ReportModel> get report => _report;
+  List<DataReport> _report = [];
+  List<DataReport> get report => _report;
+  String? error;
 
   DataState _state = DataState.loading;
   DataState get state => _state;
@@ -15,20 +23,47 @@ class ReportViewModel with ChangeNotifier {
     notifyListeners();
   }
 
-  void addReport(ReportModel report) {
-    try {
-      changeState(DataState.loading);
-      _report.add(report);
-      notifyListeners();
-      changeState(DataState.succes);
-    } catch (e) {
-      print(e.toString());
-      changeState(DataState.error);
-    }
-  }
+  Future<void> getReport() async {
+    changeState(DataState.loading);
 
-  void loadReport() {
-    List<ReportModel> data = [];
-    data = _report;
+    Jwt? tokenModel;
+    final pref = await SharedPreferences.getInstance();
+    var tokenString = pref.getString('token');
+    if (tokenString != null) {
+      var tokenList = jsonDecode(tokenString);
+      tokenModel = Jwt.fromJson(tokenList);
+
+      try {
+        changeState(DataState.loading);
+
+        var response = await ReportApi().report(tokenModel.accessToken!);
+        if (response.statusCode == 200) {
+          ReportModel responseReport = ReportModel.fromJson(response.data);
+          List<DataReport> reportList = responseReport.data!;
+          _report = reportList;
+          notifyListeners();
+          changeState(DataState.succes);
+        }
+      } on DioError catch (e) {
+        changeState(DataState.error);
+        if (e.response!.statusCode != 503) {
+          ErrorModel errorData = ErrorModel.fromJson(e.response!.data);
+          error = errorData.error;
+          debugPrint(error);
+        } else {
+          error = e.response!.statusCode!.toString() + ' Service Unavailable';
+          debugPrint(e.message.toString());
+        }
+        notifyListeners();
+      }
+    }
+    // try {
+    //   _report.add(report);
+    //   notifyListeners();
+    //   changeState(DataState.succes);
+    // } catch (e) {
+    //   print(e.toString());
+    //   changeState(DataState.error);
+    // }
   }
 }
