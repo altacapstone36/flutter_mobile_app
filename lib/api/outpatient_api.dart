a1928239e3db9304f31f0acfd88c69b10191e756
ali-susanto <alisusanto.main@gmail.com>
2022-07-09 17:08:23 +0700

    add condition to get nurse based on roles from doctor login

--- lib/api/outpatient_api.dart
+++ lib/api/outpatient_api.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../model/outpatient/process_model.dart';
+import '../model/user/model_user_login.dart';
 
 class OutpatientApi {
   final dio = Dio();
@@ -14,8 +18,25 @@ class OutpatientApi {
   }
 
   Future<Response> getNurse(String token) async {
+    DataLogin? userData;
+    final userPref = await SharedPreferences.getInstance();
+    var userString = userPref.getString('user');
+
+    var userList = jsonDecode(userString!);
+    userData = DataLogin.fromJson(userList);
+    String facility = userData.facility!;
+    int facilityId = 0;
+    if (facility == 'General') {
+      facilityId = 1;
+    } else if (facility == 'Pediatrician') {
+      facilityId = 2;
+    } else if (facility == 'Dentist') {
+      facilityId = 3;
+    }
+
+    userData = DataLogin.fromJson(userList);
     Response response = await dio.get(baseUrl + '/user',
-        queryParameters: {'role_id': 3, 'facility_id': 1},
+        queryParameters: {'role_id': 3, 'facility_id': facilityId},
         options: Options(headers: {'Authorization': 'Bearer: $token'}));
     return response;
   }
